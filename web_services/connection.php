2136fca31b29afe2a77aa407934fcad3ff720605
alapalapa <roberto-750@hotmail.com>
2014-07-06 00:27:30 -0500

    some redundances fixed, test is ready

--- web_services/connection.php
+++ web_services/connection.php
@@ -5,18 +5,21 @@ require_once 'constants.php';
 class Connection
 {
 
-	public function connect()
+	private static function connect()
 	{
 		$link = mysqli_connect(HOST, USER, PASS) or die ('there is no connection');
 		mysqli_select_db($link, DB) or die ('there is no selected database');
+
 		return $link;
 	}
 
 	public static function test()
 	{
-		$link = $this->connect();
+		$con = Connection::connect();
 		$sql = "select login from user";
-		$result = mysqli_query($link, $sql) or die ('query did not success');
+		$result = mysqli_query($con, $sql) or die ('query did not success');
+		$result = mysqli_fetch_assoc($result);
+		
 		return $result;
 	}
 }
